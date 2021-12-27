33a5a9869584b8b9b0b3e6f8178008f32235608a
haridulal1000 <84311145+haridulal1000@users.noreply.github.com>
2021-12-28 03:19:37 +0545

    pretty

--- js/Helper.js
+++ js/Helper.js
@@ -1,26 +1,25 @@
-function distance(x1,y1,x2,y2){
-    let x=x1-x2;
-    let y=y1-y2;
-    let distSq=x*x+y*y;
-    return Math.sqrt(distSq);
-
+function distance(x1, y1, x2, y2) {
+  let x = x1 - x2;
+  let y = y1 - y2;
+  let distSq = x * x + y * y;
+  return Math.sqrt(distSq);
 }
-function random(min,max){
-    return min+Math.random()*(max-min);
+function random(min, max) {
+  return min + Math.random() * (max - min);
 }
-function generateElement(){
-    const RADIUS_UPPER=10;
-    const RADIUS_LOWER=5;
-    const PADDING=5;
-    const VELOCITY_LIMIT=5;
-    const radius=random(RADIUS_LOWER,RADIUS_UPPER);
-    const x=random(radius+PADDING,width-radius-PADDING);
-    const y=random(radius+PADDING,height-radius-PADDING);
-    const xVel=random(-VELOCITY_LIMIT,VELOCITY_LIMIT);
-    const yVel=random(-VELOCITY_LIMIT,VELOCITY_LIMIT);
-    const red=random(0,255);
-    const green=random(0,255);
-    const blue=random(0,255);
-    const color={red,green,blue};
-    return{radius,x,y,xVel,yVel,color};
+function generateElement() {
+  const RADIUS_UPPER = 10;
+  const RADIUS_LOWER = 5;
+  const PADDING = 5;
+  const VELOCITY_LIMIT = 5;
+  const radius = random(RADIUS_LOWER, RADIUS_UPPER);
+  const x = random(radius + PADDING, width - radius - PADDING);
+  const y = random(radius + PADDING, height - radius - PADDING);
+  const xVel = random(-VELOCITY_LIMIT, VELOCITY_LIMIT);
+  const yVel = random(-VELOCITY_LIMIT, VELOCITY_LIMIT);
+  const red = random(0, 255);
+  const green = random(0, 255);
+  const blue = random(0, 255);
+  const color = { red, green, blue };
+  return { radius, x, y, xVel, yVel, color };
 }
